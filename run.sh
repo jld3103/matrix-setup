a3f8d9e88f6b61d1c1228294a31cf61def2f7c90
jld3103 <jld3103yt@gmail.com>
2021-01-24 22:06:49 +0100

    Only run enable services

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker-compose up
+
+SERVICES=(synapse element)
+
+TELEGRAM_ENABLE="$(yq -r .telegram_enable config.yaml)"
+if [ "$TELEGRAM_ENABLE" == "true" ]; then
+  SERVICES+=(telegram)
+fi
+
+WHATSAPP_ENABLE="$(yq -r .whatsapp_enable config.yaml)"
+if [ "$WHATSAPP_ENABLE" == "true" ]; then
+  SERVICES+=(whatsapp)
+fi
+
+docker-compose up "$(printf " %s" "${SERVICES[@]}")"
